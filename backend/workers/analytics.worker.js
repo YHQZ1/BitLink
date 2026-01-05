33c600cd8fbcf98503f541a3de446d33298baad7
Uttkarsh <rupareluttkarsh2309@gmail.com>
2026-01-05 14:51:32 +0530

    queue enx bug fix

--- backend/workers/analytics.worker.js
+++ backend/workers/analytics.worker.js
@@ -1,6 +1,3 @@
-import dotenv from "dotenv";
-dotenv.config({ path: "../.env.production" });
-
 import { Worker } from "bullmq";
 import { UAParser } from "ua-parser-js";
 import geoip from "geoip-lite";
